d88944d99d6a59ada406f074fc00521e1ec6e9a6
Nickolas Shtayn <nickolasshtayn@protonmail.com>
2025-11-12 11:44:45 -0600

    Complete select component challenge

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,47 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <Field value={value} onChange={onChange}>
+        {children}
+      </Field>
+      <FieldIcon id={"chevron-down"} size={20} strokeWidth={2}></FieldIcon>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+  background-color: ${COLORS.transparentGray15};
+  padding-block: 12px;
+  padding-inline: 16px;
+  border-radius: 8px;
+`;
+
+const Field = styled.select`
+  appearance: none;
+  border: none;
+  width: fit-content;
+  margin-inline-end: 24px;
+  background-color: transparent;
+  color: ${COLORS.gray700};
+`;
+
+const FieldIcon = styled(Icon)`
+  position: absolute;
+  right: 8px;
+  top: 11px;
+  color: ${COLORS.gray700};
+  }
+`;
 
 export default Select;
